c5d540e8175d101d09e27a7711e5034fd6d98b3d
Leonardo Telles <leo.c.telles@gmail.com>
2020-08-10 15:58:01 -0300

    Update Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
@@ -6,7 +7,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <h1>Categorias</h1>
+
+    <ul>
+      <li>
+        <Link to="/roupas">Roupas</Link>
+      </li>
+      <li>
+        <Link to="/carros">Carros</Link>
+      </li>
+      <li>
+        <Link to="/restaurantes">Restaurantes</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
