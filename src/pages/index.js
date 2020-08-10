61f2581c393d6048c37a771601df97315871a0fb
Leonardo Telles <leo.c.telles@gmail.com>
2020-08-10 17:04:54 -0300

    Update Home with categories

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+
+const DivCategories = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export const CategoryStyle = styled.div`
+  margin: 10px;
+  padding: 10px;
+  width: 201px;
+  height: 255px;
+  background: #f1f1f1;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+`
+
+const categories = [
+  {
+    title: "Roupas",
+  },
+  {
+    title: "Carros",
+  },
+  {
+    title: "Restaurantes",
+  },
+]
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Categorias</h1>
 
-    <ul>
-      <li>
-        <Link to="/roupas">Roupas</Link>
-      </li>
-      <li>
-        <Link to="/carros">Carros</Link>
-      </li>
-      <li>
-        <Link to="/restaurantes">Restaurantes</Link>
-      </li>
-    </ul>
+    <DivCategories>
+      {categories.map(category => (
+        <CategoryStyle>
+          <h3>{category.title}</h3>
+          <ul>
+            <li>Subcategoria 01</li>
+            <li>Subcategoria 02</li>
+            <li>Subcategoria 03</li>
+            <li>Subcategoria 04</li>
+          </ul>
+        </CategoryStyle>
+      ))}
+    </DivCategories>
   </Layout>
 )
 
